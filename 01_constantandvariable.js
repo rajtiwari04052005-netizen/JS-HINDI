4671a7785b2d9826ef2662fbb9155fdcb99d6311
Raj Tiwari <rajtiwari04052005@gmail.com>
2026-02-01 20:10:18 +0000

    container configdone type

---
+++ 01_constantandvariable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "rajtiwari04.com"
+var accountPassward = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+/* prefer not to use var 
+because of issue in block scope and functinal scope*/
+// const accountId = 124;
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassward,accountCity,accountState]);
---
+++ 02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as never version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readibility should be high
+
+
+console.log("raj tiwari")
+
+let name = "Raj Tiwari"
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+// number => 2 to power 53
+// bigint use a share market
+// String => ""
+// boolean => true/false
+// null => standalone value (type of maei object hota hai)
+// undefined => 
+// symbol => unique and use a react
+
+// object 
+
+console.log(typeof 5);
+console.log(typeof null);
+console.log(typeof undefined);
